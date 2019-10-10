44eb6f3c7c48010b7e7791ab8e6c6b3fb6c83d5b
Andreas Madsen <amwebdk@gmail.com>
2019-10-10 22:57:12 +0200

    make small model more stable

--- experiment/v6-exportable-small-model.py
+++ experiment/v6-exportable-small-model.py
@@ -17,12 +17,12 @@ model.add(keras.Input(shape=(50, 4), name='acceleration'))
 model.add(MaskLastFeature())
 model.add(DirectionFeatures())
 
-model.add(MaskedConv(14, 3, padding='same'))
+model.add(MaskedConv(14, 5, padding='same'))
 model.add(keras.layers.Dropout(0.1))
 
 model.add(keras.layers.LayerNormalization(scale=False))
 model.add(keras.layers.Activation('relu'))
-model.add(MaskedConv(5, 3, padding='same'))
+model.add(MaskedConv(len(dataset.classnames), 3, padding='same'))
 model.add(keras.layers.Dropout(0.1))
 
 model.add(keras.layers.GlobalMaxPooling1D())
@@ -31,10 +31,9 @@ model.add(keras.layers.Dense(len(dataset.classnames), use_bias=False))
 model.compile(optimizer=keras.optimizers.Adam(),
               loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=[keras.metrics.SparseCategoricalAccuracy()])
-
 history = model.fit(dataset.train.x, dataset.train.y,
                     batch_size=dataset.train.x.shape[0],
-                    epochs=2486,
+                    epochs=488,
                     validation_data=(dataset.validation.x, dataset.validation.y))
 
 print(
